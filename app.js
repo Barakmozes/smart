0122adc3e2f3daee8ffbfb2802943fc3fd8d8801
Barakmozes <barakmozes10@gmail.com>
2026-02-22 21:59:40 +0200

    fix app.js

--- app.js
+++ app.js
@@ -6,43 +6,30 @@ const http = require("http");
 const cors = require("cors");
 const db = require("./db/mongoConnect");
 
-const {routesInit} = require("./routes/configRoutes");
-
+const { routesInit } = require("./routes/configRoutes");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
 
-  
-
-// נותן אפשרות לכל דומיין לעשות בקשות לשרת שלנו
+// נותן אפשרות לכל דומיין לעשות בקשות לשרת שלנו (קריטי כדי ש-Netlify יוכל לדבר עם השרת הזה)
 app.use(cors());
 
 // מגדיר לשרת שהוא יכול לקבל מידע מסוג ג'ייסון בבאדי בבקשות שהם לא גט
 app.use(express.json());
 
-// דואג שתקיית פאבליק כל הקבצים בה יהיו חשופים לצד לקוח
-app.use(express.static(path.join(__dirname,"public")));
+// דואג שתקיית פאבליק כל הקבצים בה יהיו חשופים לצד לקוח (עבור קבצים של השרת עצמו)
+app.use(express.static(path.join(__dirname, "public")));
 
-// פונקציה שמגדירה את כל הראוטים הזמנים באפליקציית
-// צד שרת שלנו
+// פונקציה שמגדירה את כל הראוטים באפליקציית צד שרת שלנו
 routesInit(app);
-// אנסה לבדוק אם אנחנו על שרת אמיתי ויאסוף את הפורט משם אם לא ואנחנו לוקאלי יעבוד על 3002
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+
 // הגדרת שרת עם יכולות אפ שמייצג את האקספרס
 const server = http.createServer(app);
+
 // משתנה שיגדיר על איזה פורט אנחנו נעבוד
-
+// Render יספק PORT אוטומטית למשתנה סביבה, אחרת נעבוד על 5000 לוקאלי
 let port = process.env.PORT || 5000;
-// הפעלת השרת והאזנה לפורט המבוקש
-server.listen(port);
 
+// הפעלת השרת והאזנה לפורט המבוקש
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
